807d44f78ab4fa45ba288f7857f1c9b620d6f0cd
Anaf02 <100018619+Anaf02@users.noreply.github.com>
2025-04-24 12:39:59 +0300

    refactor create mutation

--- app/graphql/mutations/create_todo.rb
+++ app/graphql/mutations/create_todo.rb
@@ -2,13 +2,21 @@
 
 module Mutations
   class CreateTodo < BaseMutation
+    description "Creates todo with specified name and/or completed status"
+
     argument :name, String, required: true
     argument :completed, Boolean, required: false
 
     type Types::TodoType
 
     def resolve(name: nil, completed: false)
-      Todo.create!(name: name, completed: completed)
+      todo = Todo.create(name: name, completed: completed)
+
+      unless todo.save
+        raise GraphQL::ExecutionError.new "Error creating todo", extensions: todo.errors.to_hash
+      end
+
+      todo
     end
   end
 end
