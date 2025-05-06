2fa061851a3d4c9ceaa5a1986d5394a6b9311a7f
Anaf02 <100018619+Anaf02@users.noreply.github.com>
2025-05-06 10:45:22 +0300

    Delete all completed mutation

---
+++ app/graphql/mutations/delete_all_todos.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Mutations
+  class DeleteAllTodos < BaseMutation
+    description "Deletes all filtered todos (completed=true)"
+
+    field :message, String, null: false
+
+    argument :completed, Boolean, required: true
+
+    def resolve(completed:)
+      raise GraphQL::ExecutionError, "Only completed=true is allowed" unless completed
+
+      todo_manager = TodoManager.new
+      result = todo_manager.destroy_all_completed_todos(completed: completed)
+
+      if result.success?
+        { message: "#{result.todo} todo(s) have been deleted" }
+      else
+        raise GraphQL::ExecutionError.new "Error deleting all completed todos", extensions: result.errors.to_hash
+      end
+    end
+  end
+end
