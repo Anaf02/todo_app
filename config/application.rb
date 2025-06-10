a46893a39b2bc88c067f3bfd929ff5d082f578df
Anaf02 <100018619+Anaf02@users.noreply.github.com>
2025-06-10 15:33:03 +0300

    Fix copilot deploy

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,17 @@ module TodoApp
       current_graphql_field: -> { GraphQL::Current.field&.path },
       current_dataloader_source: -> { GraphQL::Current.dataloader_source_class },
     ]
+    #
+    # config.hosts = [
+    #   IPAddr.new("0.0.0.0/0"), # All IPv4 addresses.
+    #   IPAddr.new("::/0"), # All IPv6 addresses.
+    #   "localhost", # The localhost reserved domain.
+    #   ENV["RAILS_HOST_AWS"]
+    # ]
+
+    config.hosts << "todo-a-publi-mjhslaykblcm-902164618.eu-central-1.elb.amazonaws.com"
+    config.hosts << "todo-a-publi-zch9zpbhcfa5-1123118274.eu-central-1.elb.amazonaws.com"
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
